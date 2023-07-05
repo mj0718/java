5fda2efa1039a534fc3f67824bfb7b20b4d4be61
mj0718 <109218705+mj0718@users.noreply.github.com>
2023-07-05 11:33:41 +0900

    first-spring-maven

---
+++ first-spring-maven/src/main/java/himedia/firstspringmaven/FirstSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package himedia.firstspringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringMavenApplication.class, args);
+	}
+
+}
